72ac1fd7c72c8abd1dbc1f71efaf3b2a265d2caf
Roman Efremenko <roman.efremenko@infowatch.com>
2024-12-15 22:20:11 +0300

    ci: configure coverage action

--- crates/octa-cli/src/lib.rs
+++ crates/octa-cli/src/lib.rs
@@ -138,7 +138,6 @@ pub async fn run() -> OctaResult<()> {
     }
   });
 
-  // List all tasks and exit
   if args.list_tasks {
     let finder = OctaFinder::new();
     let commands = finder.find_by_path(Arc::clone(&octafile), "**");
