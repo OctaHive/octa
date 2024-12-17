407ed2dd847245e10e3c5d77356b031c3d05dfac
Roman Efremenko <roman.efremenko@infowatch.com>
2024-12-17 08:48:51 +0300

    feat: add possiblity for simple task

--- crates/octa-octafile/src/task.rs
+++ crates/octa-octafile/src/task.rs
@@ -1,6 +1,9 @@
 use std::{collections::HashMap, path::PathBuf};
 
-use serde::{Deserialize, Serialize};
+use serde::{
+  de::{MapAccess, Visitor},
+  Deserialize, Deserializer, Serialize,
+};
 use serde_yml::Value;
 
 use crate::{Cmds, Vars};
@@ -97,7 +100,7 @@ impl From<String> for Deps {
   }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Debug, Clone, Default)]
 pub struct Task {
   pub dir: Option<PathBuf>,                      // Working directory for the task
   pub desc: Option<String>,                      // Task description
@@ -115,4 +118,67 @@ pub struct Task {
   pub sources: Option<Vec<String>>,              // Sources for fingerprinting
   pub source_strategy: Option<SourceStrategies>, // Strategy for compare sources
   pub predonditions: Option<Vec<String>>,        // Commands to check should run command
+}
+
+impl<'de> Deserialize<'de> for Task {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    struct TaskVisitor;
+
+    impl<'de> Visitor<'de> for TaskVisitor {
+      type Value = Task;
+
+      fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a string or a map representing a Task")
+      }
+
+      fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+      where
+        E: serde::de::Error,
+      {
+        Ok(Task {
+          cmd: Some(Cmds::Simple(value.to_string())),
+          ..Task::default()
+        })
+      }
+
+      fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+      where
+        M: MapAccess<'de>,
+      {
+        let mut task = Task::default();
+
+        while let Some(key) = map.next_key::<String>()? {
+          match key.as_str() {
+            "dir" => task.dir = map.next_value()?,
+            "desc" => task.desc = map.next_value()?,
+            "vars" => task.vars = map.next_value()?,
+            "tpl" => task.tpl = map.next_value()?,
+            "cmd" => task.cmd = map.next_value()?,
+            "cmds" => task.cmds = map.next_value()?,
+            "internal" => task.internal = map.next_value()?,
+            "platforms" => task.platforms = map.next_value()?,
+            "ignore_error" => task.ignore_error = map.next_value()?,
+            "deps" => task.deps = map.next_value()?,
+            "run" => task.run = map.next_value()?,
+            "silent" => task.silent = map.next_value()?,
+            "execute_mode" => task.execute_mode = map.next_value()?,
+            "sources" => task.sources = map.next_value()?,
+            "source_strategy" => task.source_strategy = map.next_value()?,
+            "predonditions" => task.predonditions = map.next_value()?,
+            _ => {
+              // Skip unknown fields
+              let _ = map.next_value::<serde::de::IgnoredAny>()?;
+            },
+          }
+        }
+
+        Ok(task)
+      }
+    }
+
+    deserializer.deserialize_any(TaskVisitor)
+  }
 }
